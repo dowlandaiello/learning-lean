d5cbfff0281e150db30fd76c2a90b606608678a3
Dowland Aiello <dowlandaiello@gmail.com>
2024-12-26 02:46:55 -0500

    Add filter combinator.

--- LearningLean/Parsing.lean
+++ LearningLean/Parsing.lean
@@ -9,15 +9,27 @@ def Parser (α β : Type) := α -> Except Error β
 
 -- Matches a specific element, and produces some output after parsing
 def just {α : Type} [BEq α] [ToString α] : α -> Parser α α
-  | a => fun e => if e == a then Except.ok e else Except.error s!"found {e}; expected {a}"
+  | a => fun e => if e == a then pure e else Except.error s!"found {e}; expected {a}"
 
-theorem just_matches_x_with_x { α : Type } [BEq α] [ToString α] (x : α) (h : x == x) : just x x = Except.ok x := by
-  unfold just
-  simp [eq_self]
-  exact h
+def filterExpected {α : Type} [ToString α] : (α -> Bool) -> Option String -> Parser α α
+  | f, expected => fun e => if f e then pure e else Except.error s!"predicate match failed: found {e}; expected {expected}"
 
-theorem just_does_not_match_a_with_b { α : Type } [BEq α] [LawfulBEq α] [ToString α] (a : α) (b : α) (h : a ≠ b) : just a b = Except.error s!"found {b}; expected {a}" := by
+
+theorem just_matches_x_with_x {α : Type} [BEq α] [ReflBEq α] [ToString α] (x : α) : just x x = pure x := by
   unfold just
-  simp [← ne_eq]
-  symm
-  exact h
+  simp [← BEq.refl]
+  simp
+
+theorem just_does_not_match_a_with_b {α : Type} [BEq α] [ToString α] (a : α) (b : α) (h : ¬(b == a)) : just a b = Except.error s!"found {b}; expected {a}" := by
+  unfold just
+  simp [h]
+
+theorem filter_matches_in_with_success_f {α : Type} [ToString α] (a : α) (f : (α -> Bool)) (h : f a) : filterExpected f Nothing a = pure a := by
+  unfold filterExpected
+  simp
+  simp [h]
+
+theorem filter_does_not_match_in_with_bad_f {α : Type} [ToString α] (a : α) (f : (α -> Bool)) (h : ¬(f a)) : filterExpected f Nothing a = Except.error s!"predicate match failed: found {a}; expected {Nothing}" := by
+  unfold filterExpected
+  simp
+  simp [h]
